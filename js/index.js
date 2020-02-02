e9a1361ca7192b14dca300aaa73dcc4de7bbb118
BinaryRx <Binaryrx@gmail.com>
2020-02-02 17:30:06 +0200

    latest

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 window.onscroll = (e) =>{
     let scrollDistance = window.scrollY;
     const header = document.querySelector('header');
+    const instaLogo = document.querySelector('.instagramLogo')
     const sidebar = document.querySelector('.sidebar');
     const container = document.querySelector('.main_container');
     const mediaQuery = window.matchMedia( "(min-width: 1000px)" );
@@ -10,6 +11,7 @@ window.onscroll = (e) =>{
     if(scrollDistance >= 100){
       
       header.style.height = '52px';
+      instaLogo.style.opacity = '0';
       sidebar.classList.add('fixed');
 
         if(mediaQuery.matches){
@@ -20,6 +22,7 @@ window.onscroll = (e) =>{
 
     }else{
       header.style.height = '77px';
+      instaLogo.style.opacity = '1';
       sidebar.classList.remove('fixed');
 
     }
